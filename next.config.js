f4287ac0e1a2760ef2d4b415b53b05a74d43360a
dhruvvv-kush <dhruvkushwaha90@gmail.com>
2026-08-02 02:39:10 +0530

    Configure Next.js static HTML export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/ELAN',
   images: {
     unoptimized: true,
   },
   reactStrictMode: true,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
